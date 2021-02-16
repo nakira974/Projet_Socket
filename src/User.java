61ba0b0cb50d7ebe1992723b9b7e905ffb62489f
nakira974 <maxime.loukhal@gmail.com>
2021-02-16 13:55:31 +0100

    [STABLE] modif SQL  Principale.main   -- > Maj user_admin 16/02/2021

--- src/User.java
+++ src/User.java
@@ -9,17 +9,22 @@ class LogUser {
 
     }
 
-    public SocketPerso login (ArrayList<String> args) {
+    public SocketPerso login (ArrayList<String> args) throws SQLException {
         SocketPerso socket_client = null;
+        ResultSet rs = null;
         try {
             String url = "jdbc:msql://localhost/serveur_db";
             Connection conn = DriverManager.getConnection(url,"ServerMaster","Master2004$");
             Statement stmt = conn.createStatement();
-            ResultSet rs;
+
 
             rs = stmt.executeQuery(
                     "SELECT pseudo, dt_last_connection " +
                             "FROM user WHERE password ="+ args.get(1) + " AND pseudo="+ args.get(0));
+            if(!rs.next()){
+                System.err.println("Nom d'utilisateur ou mot de passe incorrect(s) ! ");
+                return null;
+            }
             while ( rs.next() ) {
                 String pseudo = rs.getString("pseudo");
                 System.out.println(pseudo);
@@ -31,7 +36,7 @@ class LogUser {
             Socket_Serveur.users.put(currentUser, socket_client.getSocket());
             conn.close();
         } catch (Exception e) {
-            System.err.println("Got an exception! ");
+            System.err.println("Erreur d'authenfication ! ");
             System.err.println(e.getMessage());
         }
         return socket_client;
