cef352dcc344b423b445c21f94d8146baa51d79d
Weefle <mr.weefle@gmail.com>
2022-02-04 17:14:38 +0100

    Added read and write files for both client and serveur with JSON

--- src/SocketPerso.java
+++ src/SocketPerso.java
@@ -155,15 +155,7 @@ class Socket_Serveur {
 
 
     }
-    public static int[] convertIntegers(List<Integer> integers)
-    {
-        int[] ret = new int[integers.size()];
-        for (int i=0; i < ret.length; i++)
-        {
-            ret[i] = integers.get(i).intValue();
-        }
-        return ret;
-    }
+
 }
 
 
